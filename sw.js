86786b23841b328ca9356349fe904d285e90d953
Hadionlineclas <167676507+Hadionlineclas@users.noreply.github.com>
2026-05-07 16:13:41 +0500

    Update service worker domain and zone ID

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10975548
+    "domain": "3nbf4.com",
+    "zoneId": 10975559
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
